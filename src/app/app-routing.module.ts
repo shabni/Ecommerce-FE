0a700c6452ba18542967502ea25815c0e0836e21
Ahsan Ahmad <ahsan.ahmad@ovada.com>
2024-11-26 12:14:42 +0500

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductComponent } from './product/product.component';
+import { DetailsComponent } from './details/details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'products', component: ProductComponent },
+  { path: 'product/:id', component: DetailsComponent },
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  { path: '**', redirectTo: '/products', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product',
@@ -9,36 +10,42 @@ export class ProductComponent implements OnInit {
 
   products = [
     {
+      id:'1',
       name: '2 Piece Khaddar Suit - Printed',
       type: 'Unstitched',
       price: 1999,
       image: 'assets/products/background-1.jpg',
     },
     {
+      id:'2',
       name: '2 Piece Marina Suit - Printed',
       type: 'Unstitched',
       price: 3899,
       image: 'assets/products/background-2.jpg',
     },
     {
+      id:'3',
       name: '2 Piece Stone Suit - Printed',
       type: 'Unstitched',
       price: 3699,
       image: 'assets/products/background-3.jpg',
     },
     {
+      id:'4',
       name: '2 Piece Stone Suit - Printed',
       type: 'Unstitched',
       price: 3699,
       image: 'assets/products/background-4.jpg',
     },
     {
+      id:'5',
       name: '2 Piece Stone Suit - Printed',
       type: 'Unstitched',
       price: 3699,
       image: 'assets/products/background-5.jpg',
     },
     {
+      id:'6',
       name: '2 Piece Stone Suit - Printed',
       type: 'Unstitched',
       price: 3699,
@@ -47,9 +54,13 @@ export class ProductComponent implements OnInit {
     // Add more product objects as needed
   ];
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  goToProductDetail(id: string) {
+    this.router.navigate(['/product', id]);
   }
 
 }
